5c654bab3a4a91f98d08e3cb56f1190280ff8916
Jaysingh Patel <153704686+Jaysingh2003@users.noreply.github.com>
2025-06-06 22:40:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Settings from './pages/Settings'
+import Profile from './pages/Profile'
+import Help from './pages/Help'
+import NotFound from './pages/NotFound'
+
+function App() {
+  return (
+    
+    <Routes>
+      <Route path="/" element={<Dashboard/>} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/help" element={<Help />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+   
+  )
+}
+
+export default App
